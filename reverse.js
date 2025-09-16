58fd8f2e0e9879162585ceb748033d0457c5b4ae
Sowmiya255 <sowmiyaramasamy489@gmail.com>
2025-09-17 05:11:34 +0530

    edit

---
+++ reverse.js
@@ -0,0 +1,11 @@
+// function reversestring (str) {
+//     return str.split("").reverse().join("")
+// }
+
+// console.log(reversestring("word"));
+
+const reversestring = str =>{
+    return str.split("").reverse().join("")
+}
+
+console.log(reversestring("sowmiya"));
