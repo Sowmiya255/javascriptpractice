8750690a893401bd759d5ced592b99eeaf822e16
Sowmiya255 <sowmiyaramasamy489@gmail.com>
2025-09-17 05:52:17 +0530

    edit

---
+++ palindrome.js
@@ -0,0 +1,26 @@
+// function palindrome (str){
+//     const reversed = str.split("").reverse().join("");
+
+//     if(str==reversed){
+//         console.log("it is palindrome");
+//     }
+//     else {
+//         console.log("not a palindrome");
+//     }
+// }
+
+// console.log(palindrome("wow"));
+
+const prompt = require("prompt-sync")();
+
+const plaindrome =(name)=>{
+    const reversed =name.split("").reverse().join("")
+    if(name == reversed){
+        return "it is a pailndrome";}
+        else {
+        return "not a palindrome";
+    }
+}
+const name = prompt("enter the input:");
+
+console.log(plaindrome(name));
